ceb89da7ab63ffc1d3fac1c60aa8360a65a17f0a
BitanSarkar007 <82215438+BitanSarkar007@users.noreply.github.com>
2024-04-20 05:23:53 +0530

    Update vulnerables.sh

--- vulnerables.sh
+++ vulnerables.sh
@@ -14,10 +14,10 @@ DIRECTORY="vulhub"
 
 #CONTAINERS[0]="$DIRECTORY/coldfusion/CVE-2017-3066/docker-compose.yml"
 #CONTAINERS[1]="$DIRECTORY/openssl/CVE-2014-0160/docker-compose.yml"
-#CONTAINERS[0]="$DIRECTORY/activemq/CVE-2016-3088/docker-compose.yml"
+CONTAINERS[0]="$DIRECTORY/activemq/CVE-2016-3088/docker-compose.yml"
 CONTAINERS[1]="$DIRECTORY/grafana/CVE-2021-43798/docker-compose.yml" 
 #CONTAINERS[4]="$DIRECTORY/cacti/CVE-2022-46169/docker-compose.yml"
-CONTAINERS[2]="$DIRECTORY/apisix/CVE-2021-45232/docker-compose.yml"
+# ONTAINERS[2]="$DIRECTORY/apisix/CVE-2021-45232/docker-compose.yml"
 
 # Randomly choose six vulnerable containers to start
 #random () {
