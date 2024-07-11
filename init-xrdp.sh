8a1b63d75bc69652e4c694bf7d0e33a7c1ee9948
BitanSarkar007 <82215438+BitanSarkar007@users.noreply.github.com>
2024-07-12 04:42:00 +0530

    Create init-xrdp.sh

---
+++ init-xrdp.sh
@@ -0,0 +1,12 @@
+sudo useradd -s /bin/bash -d /opt/bit -m bit
+sudo usermod -aG sudo bit
+
+sudo -u bit -i
+
+sudo apt update
+sudo apt install xfce4 xfce4-goodies -y
+sudo apt install xrdp -y
+sudo systemctl start xrdp
+cd ~
+echo "xfce4-session" | tee .xsession
+sudo systemctl restart xrdp
