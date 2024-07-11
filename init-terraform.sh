57b31b6eb2600e198adcb4d7f4f2cb30e8524d4b
BitanSarkar007 <82215438+BitanSarkar007@users.noreply.github.com>
2024-07-11 16:55:36 +0530

    Update init-terraform.sh

--- init-terraform.sh
+++ init-terraform.sh
@@ -8,4 +8,4 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt update
-sudo apt-get install terraform
+sudo apt-get install terraform -y
